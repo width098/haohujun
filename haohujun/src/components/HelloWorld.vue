ed436d085f9bb1f7fda965d94843c2456e51081f
width098 <2562619447@qq.com>
2021-10-20 12:38:12 +0800

    吃饭取

--- haohujun/src/components/HelloWorld.vue
+++ haohujun/src/components/HelloWorld.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+     <h1>学习</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      1231242412
+    
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
