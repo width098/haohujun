dd722c88d4991a38d73333db71a1638d0971b7d1
width098 <2562619447@qq.com>
2021-10-20 11:26:30 +0800

    第六次提交了

--- haohujun/src/App.vue
+++ haohujun/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/yangmi.jpg">
+    <HelloWorld msg="欢迎回家,大少爷"/>
   </div>
 </template>
 
